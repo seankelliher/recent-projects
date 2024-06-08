7b2f255b3076eb3793614fec0ea57ff34aaa57ed
Sean Kelliher <sbkelliher@icloud.com>
2024-06-08 18:48:50 -0400

    Modify 'open in new tab' functionality.

--- src/composables/go-to-links.js
+++ src/composables/go-to-links.js
@@ -1,13 +1,13 @@
 function openGitHub(repo) {
-    window.open(`https://github.com/seankelliher/${repo}`, "gitHubTab");
+    window.open(`https://github.com/seankelliher/${repo}`, "_blank");
 }
 
 function openNetlify(repo) {
-    window.open(`https://sean-kelliher-${repo}.netlify.app`, "netlifyTab");
+    window.open(`https://sean-kelliher-${repo}.netlify.app`, "_blank");
 }
 
 function openHeroku(repo) {
-    window.open(`https://${repo}.herokuapp.com/`, "herokuTab");
+    window.open(`https://${repo}.herokuapp.com/`, "_blank");
 }
 
 export { openGitHub, openNetlify, openHeroku };
