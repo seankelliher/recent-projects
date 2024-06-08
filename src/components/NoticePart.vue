77629f6101e674786ce80577fa0d62383ce80ae4
Sean Kelliher <sbkelliher@icloud.com>
2024-06-08 18:56:20 -0400

    Add tabindex and keyup for accessibility.

--- src/components/NoticePart.vue
+++ src/components/NoticePart.vue
@@ -12,7 +12,12 @@ function hideNotice() {
     <div v-if="showNotice" class="notice">
         <p class="notice-txt">Projects hosted at Heroku use "eco dynos." They sleep to save energy and take a few seconds to wake up.</p>
 
-        <figure @click="hideNotice()" class="notice-icon">
+        <figure
+            tabindex="0"
+            @click="hideNotice()"
+            @keyup.enter="hideNotice()"
+            class="notice-icon"
+        >
             <img src="/images/close_24dp_FILL0_wght400_GRAD0_opsz24.svg" alt="icon to close interface">
         </figure>
     </div>
